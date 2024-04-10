81047bb61a0bb6c17fce691e0e92e21e97590ac4
dinesh <dineshpaldinesh3@gmail.com>
2024-04-10 19:47:12 +0530

    add amiddleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
-export const signup=async(req,res)=>{
+import { errorHandler } from '../utils/error.js'
+export const signup=async(req,res,next)=>{
     // console.log(req.body)
     const {username,email,password}=req.body
     const hashedPassword=bcryptjs.hashSync(password,10)
@@ -9,7 +10,8 @@ export const signup=async(req,res)=>{
         await newUser.save()
         res.status(201).json({message:'user created successfully'})
     } catch (error) {
-        res.status(500).json({message:error.message}) 
+        // res.status(500).json({message:error.message}) 
+        next(error)
     }
   
 }
