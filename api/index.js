728ed8f099caf64b72ce7a1c65d3b79bf098c997
dinesh <dineshpaldinesh3@gmail.com>
2024-04-14 08:35:45 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from "cookie-parser"
+import path from 'path';
 
 dotenv.config()
 mongoose.connect(process.env.MONGO)
@@ -24,7 +25,12 @@ app.listen(3000,()=>{
 //         message:"Api is working"
 //     })
 // })
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
